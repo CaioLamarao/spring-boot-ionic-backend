184c911b26119389492c5520576bbe99f9ed1381
Caio Lamarão <caiolamarao@outlook.com>
2022-08-10 16:20:56 -0300

    Criando repository e service para 'categoria'

---
+++ src/main/java/com/caiolamarao/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.caiolamarao.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.caiolamarao.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+}
