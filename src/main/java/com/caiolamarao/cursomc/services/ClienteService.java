5979c52732e233242645baf059d0feb38b63446c
Caio Lamarão <caiolamarao@outlook.com>
2022-08-12 16:13:41 -0300

    Endpoint clientes/id

---
+++ src/main/java/com/caiolamarao/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.caiolamarao.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.caiolamarao.cursomc.domain.Cliente;
+import com.caiolamarao.cursomc.repositories.ClienteRepository;
+import com.caiolamarao.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
